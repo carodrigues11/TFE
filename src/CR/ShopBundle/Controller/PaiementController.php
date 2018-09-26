0362d86c65657c7d08324318a945382b7253976b
carorodrigues <caroline@mcarnoldsdigital.com>
2018-09-26 21:33:12 +0200

    Paiement (1/2)

--- src/CR/ShopBundle/Controller/PaiementController.php
+++ src/CR/ShopBundle/Controller/PaiementController.php
@@ -19,8 +19,6 @@ class PaiementController extends Controller
     public function checkoutAction()
     {
 
-
-
         \Stripe\Stripe::setApiKey("sk_test_5LsCslQIJ4K5M6eV818LQMoU");
 
         // Get the credit card details submitted by the form
@@ -46,8 +44,48 @@ class PaiementController extends Controller
             // The card has been declined
         }
 
+    }
+
+    public function adresseAction()
+    {
+
+        $user = $this->getUser();
 
 
+        return $this->render('CRShopBundle:Paiement:adresse.html.twig', array(
+            'user'=>$user
+        ));
+
+    }
+
+    public function detailAction()
+    {
+        return $this->render('CRShopBundle:Paiement:detail.html.twig');
+
+    }
+
+    public function detailAllAction()
+    {
+
+        $request = $this->get('request_stack')->getCurrentRequest();
+        $session = $request->getSession();
+        if(!$session->has('panier')) $session->set('panier',array());
+
+        //A partir de l'ID du produit, récupérer les infos du produit ajouté
+        $em = $this->getDoctrine()->getEntityManager();
+        $produits = $em->getRepository('CRShopBundle:Produits')->findArray(array_keys($session->get('panier')));
+
+
+        return $this->render('CRShopBundle:Paiement:detailAll.html.twig', array(
+            'produits'=>$produits,
+            'panier'=>$session->get('panier')
+        ));
+
+    }
+
+    public function typeAction()
+    {
+        return $this->render('CRShopBundle:Paiement:type.html.twig');
 
     }
 
